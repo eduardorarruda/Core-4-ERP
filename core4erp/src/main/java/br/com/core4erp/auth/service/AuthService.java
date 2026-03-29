4752c1a2b824ea781c701eb0c33e050137fcfb74
João Marcello <joaomarcello@redfoxpay.com.br>
2026-03-29 11:23:33 -0300

    Criando endpoint de cadastro de usuario

--- core4erp/src/main/java/br/com/core4erp/auth/service/AuthService.java
+++ core4erp/src/main/java/br/com/core4erp/auth/service/AuthService.java
@@ -4,22 +4,28 @@ import br.com.core4erp.auth.dto.LoginRequestDto;
 import br.com.core4erp.auth.dto.LoginResponseDto;
 import br.com.core4erp.auth.entity.Auth;
 import br.com.core4erp.auth.repository.AuthRepository;
-import lombok.Data;
+import br.com.core4erp.user.entity.User;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-@Data
+@Service
 public class AuthService {
 
     private final AuthenticationManager authenticationManager;
     private final AuthRepository authRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthService(AuthenticationManager authenticationManager, AuthRepository authRepository) {
+    public AuthService(AuthenticationManager authenticationManager,
+                       AuthRepository authRepository,
+                       PasswordEncoder passwordEncoder) {
         this.authenticationManager = authenticationManager;
         this.authRepository = authRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public LoginResponseDto login(LoginRequestDto request) {
@@ -40,6 +46,24 @@ public class AuthService {
                 auth.get().getUsername(),
                 auth.get().getRole()
         );
+    }
+
+    public void register(User user, String username, String password, String role) {
+
+        Optional<Auth> usernameExists = authRepository.findByUsername(username);
+        if(usernameExists.isPresent())
+            throw new RuntimeException("Username cadastrado");
+
+        Auth auth = new Auth();
+        auth.setUsername(username);
+        auth.setPassword(passwordEncoder.encode(password));
+        if(role.isEmpty())
+            auth.setRole("normal-user");
+        auth.setRole(role);
+        auth.setUser(user);
+
+        authRepository.save(auth);
+
     }
 
 }
